181b88396771e87c2c84f52e79cda01e1eeab3f2
puancoffee <puanngopi@gmail.com>
2020-03-28 21:49:21 +0700

    add change navbar

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 $(document)
     .ready(function () {
-        $('.owl-carousel').owlCarousel({
-            loop: true,
-            smartSpeed: 900,
-            autoWidth: true,
-            center: true,
-            nav: false,
-
-        });
+        $('.owl-carousel').owlCarousel({loop: true, smartSpeed: 900, autoWidth: true, center: true, nav: false});
         $('.fa-chevron-left').click(function () {
             $('.owl-carousel').trigger('next.owl.carousel');
         });
         $('.fa-chevron-right').click(function () {
             $('.owl-carousel').trigger('prev.owl.carousel');
         });
+
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 1000) { 
+                $('.navbar .navbar-brand img').attr('src','./logo.png');
+            }
+            if ($(this).scrollTop() < 1000) { 
+                $('.navbar .navbar-brand img').attr('src','./logo.webp');
+            }
+        })
     });
+
